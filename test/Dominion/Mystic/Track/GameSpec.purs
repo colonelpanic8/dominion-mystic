3f7c817316c14983775363414cb81c101b78b4f7
Ivan Malison <IvanMalison@gmail.com>
2020-04-21 05:37:15 -0700

    And shuffling/drawing interaction test

--- test/Dominion/Mystic/Track/GameSpec.purs
+++ test/Dominion/Mystic/Track/GameSpec.purs
@@ -95,6 +95,48 @@ Turn 1 - Lord Rattington
           , c 1 "Estate"
           , c 1 "Inventor"
           ]
+      it "handles the crazy interaction between shuffling and drawing" do
+        processLines
+          """
+L starts with a Goat.
+L starts with 6 Coppers.
+L starts with 3 Estates.
+E starts with a Goat.
+E starts with 6 Coppers.
+E starts with 3 Estates.
+L shuffles their deck.
+L draws 5 cards.
+E shuffles their deck.
+E draws 4 Coppers and an Estate.
+
+Turn 1 - EyeVanMaliceSon
+E plays 4 Coppers. (+$4)
+E buys and gains an Inventor.
+E draws 2 Coppers, 2 Estates and a Goat.
+
+Turn 2 - EyeVanMaliceSon
+E plays a Goat. (+$1)
+E trashes an Estate.
+E plays 2 Coppers. (+$2)
+E buys and gains a Silver.
+E shuffles their deck.
+E draws 2 Coppers, 2 Estates and an Inventor.
+
+Turn 1 - Lord Rattington
+"""
+        deckSectionEquals "E" Data._deck []
+        deckSectionEquals "E" Data._play []
+        deckSectionEquals "E" Data._hand
+          [ c 2 "Copper"
+          , c 2 "Estate"
+          , c 1 "Inventor"
+          ]
+        deckSectionEquals "E" Data._discard
+          [ c 4 "Copper"
+          , c 1 "Inventor"
+          , c 1 "Silver"
+          , c 1 "Goat"
+          ]
   where
   it does action =
     Spec.it does $ map Tuple.fst
